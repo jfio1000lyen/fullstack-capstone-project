2b323ae3b5934154c0f906fd519080d35dfc8ee3
jfio1000lyen <jeannel93.jnm@gmail.com>
2026-06-06 19:05:06 -0400

    changes

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
-import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import LoginPage from './components/LoginPage/LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -14,8 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/Login" element={<LoginPage />} />
         </Routes>
         </>
   );
