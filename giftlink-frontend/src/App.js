4596b61f166d2aa7142c4a599e07c4122fea9df1
jfio1000lyen <jeannel93.jnm@gmail.com>
2026-06-06 23:54:23 -0400

    changes

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -16,6 +18,10 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/Login" element={<LoginPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage />} />
+          <Route path="/app/search" element={<SearchPage />} />
+
+
         </Routes>
         </>
   );
--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">GiftLink</a>
+            <li className="nav-item">
+	            <Link className="nav-link" to="/app/search">Search</Link>
+            </li>
+
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
 
