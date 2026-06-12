0e5188161bc65fae9004dea1b0415200e93955ff
jfio1000lyen <jeannel93.jnm@gmail.com>
2026-06-12 00:45:14 -0400

    Remove unused navigate import from App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -10,7 +10,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
   return (
 		<>
         <Navbar/>
