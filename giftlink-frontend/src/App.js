25c69a2428ced7983e8d76079c0177c024f0458c
jfio1000lyen <jeannel93.jnm@gmail.com>
2026-06-12 00:47:34 -0400

    Remove unused imports from App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
